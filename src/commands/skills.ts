352967a20a17e2600a9f80e7ced216010eef77de
Jeb <jesperkauppinen@gmail.com>
2022-10-02 16:48:53 +0300

    skill command

--- src/commands/skills.ts
+++ src/commands/skills.ts
@@ -18,19 +18,25 @@ export const Command: ICommand = {
     console.log(user);
     const playerSkills = user?.selectedProfile?.profile?.skills || [];
 
-    const fields: EmbedField[] = playerSkills.map(({ skill }) => ({
-      name: skill.name,
-      value: skill.name,
-      inline: true,
-    }));
+    const fields: EmbedField[] = playerSkills
+      .map((skill) => ({
+        name: `${skill.skill.emoji} ${skill.skill.name}`,
+        value: formatSkill(skill),
+        inline: true,
+        skill: skill.skill,
+      }))
+      .sort((a, b) => {
+        if (a.skill.name < b.skill.name) return -1;
+        else if (a.skill.name === b.skill.name) return 0;
+        return 1;
+      });
 
     const embeddedMsg = {
       color: 0x00aaaa,
       author: {
-        name: interaction.user.tag,
+        name: `${user?.selectedProfile?.profile?.label || 'N/A'}'s skills`,
         icon_url: interaction.user.avatarURL() + '',
       },
-      description: `${interaction.user.tag}'s skills`,
       fields: fields,
     };
 
