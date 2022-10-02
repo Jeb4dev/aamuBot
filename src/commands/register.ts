b54d3357e39c475a7b85054060f3517b5053dd92
Jeb <jesperkauppinen@gmail.com>
2022-10-02 04:13:45 +0300

    Add profile wallet skills
    Co-authored-by: MrTanoshii <47116127+MrTanoshii@users.noreply.github.com>

--- src/commands/register.ts
+++ src/commands/register.ts
@@ -21,11 +21,57 @@ export const Command: ICommand = {
       await interaction.reply(`Welcome back <@${user.id}>!`);
       console.log(user);
     } else {
-      user = usersRepository.create({
-        id: userId,
-      });
-      await usersRepository.save(user);
+      user = await createUser(userId);
+      let profile = await createProfile(user);
+      await createSelectedProfile(user, profile);
+      console.log(user);
       await interaction.reply(`Welcome <@${user.id}>!`);
     }
   },
 };
+
+async function createUser(userId: string) {
+  const userRepository = AppDataSource.getRepository(User);
+  let user = userRepository.create({ id: userId });
+  await createProfile(user);
+  return user;
+}
+
+async function createSelectedProfile(user: User, profile: Profile) {
+  const selectedProfileRepository = AppDataSource.getRepository(SelectedProfile);
+  let selectedProfile = selectedProfileRepository.create({ user: user, profile: profile });
+  await selectedProfileRepository.save(selectedProfile);
+  return selectedProfile;
+}
+
+async function createProfile(user: User) {
+  const profileRepository = AppDataSource.getRepository(Profile);
+  let profile = profileRepository.create({ user: user });
+  profile.label = 'Test Profile'; // TODO: Ask user for profile label
+  await profileRepository.save(profile);
+
+  console.warn('This is here', profile);
+  await createWallet(profile);
+  await createPlayerSkills(profile);
+  console.warn('This is there', profile);
+  return profile;
+}
+
+async function createWallet(profile: Profile) {
+  const walletRepository = AppDataSource.getRepository(Wallet);
+  let wallet = walletRepository.create({ profile: profile });
+  await walletRepository.save(wallet);
+  return wallet;
+}
+
+async function createPlayerSkills(profile: Profile) {
+  const playerSkillRepository = AppDataSource.getRepository(PlayerSkill);
+  const availableSkillsRepository = AppDataSource.getRepository(Skill);
+
+  let availableSkills: Skill[] = await availableSkillsRepository.find();
+
+  for (let key in availableSkills) {
+    let playerSkills = playerSkillRepository.create({ profile: profile, skill: availableSkills[key] });
+    await playerSkillRepository.save(playerSkills);
+  }
+}
