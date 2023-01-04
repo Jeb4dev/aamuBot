2307317cde7900be3bba13b35bb934ce2f28d782
Jeb <jesperkauppinen@gmail.com>
2023-01-04 07:44:24 +0200

    fixed mornign count

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -2,14 +2,14 @@
 
 export const CreateDate = (date: string): Date => {
   let time = new Date(date);
-  time.setHours(7) // 6 AM EET
+  time.setHours(6) // 6 AM EET
   time.getTimezoneOffset()
   return time
 }
 
 export const MorningsLeft = (date: Date): Number => {
   let now = new Date();
-  return Math.ceil((date.getTime() - now.getTime()) / 86400000) // ms in 24 h
+  return Math.ceil((date.getTime() - now.getTime()) / 86400000 - 1) // ms in 24 h
 }
 
 export const DaysLeftToString = (date: Date): String => {
@@ -17,7 +17,7 @@ export const DaysLeftToString = (date: Date): String => {
   const DayDiff = date.getDate() - now.getDate()
   const MonthDiff = date.getMonth() - now.getMonth()
   const YearDiff = date.getFullYear() - now.getFullYear()
-  let daysLeft = Math.ceil((date.getTime() - now.getTime()) / 86400000)
-  return `${daysLeft-1} days left. That is ${YearDiff==0 ? "" : YearDiff + " years"} ${MonthDiff==0 ? "" : MonthDiff + " months"} ${DayDiff==0 ? daysLeft : DayDiff + " days"}`
+  let daysLeft = Math.ceil((date.getTime() - now.getTime()) / 86400000 - 1)
+  return `${daysLeft} days left. That is ${YearDiff==0 ? "" : YearDiff + " years"} ${MonthDiff==0 ? "" : MonthDiff + " months"} ${DayDiff==0 ? daysLeft : DayDiff + " days"}`
 }
 
